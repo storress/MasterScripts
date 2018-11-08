5305d8ad50ed8de19cea0de38fbffaf2f898e8c9
Salomon Torres <nomolas1@gmail.com>
2018-11-08 16:15:57 +0000

    Filter routes with 100ms and add m/s colum to the csv output file

--- get_specific_gps_traces.py
+++ get_specific_gps_traces.py
@@ -9,7 +9,7 @@ bottomPositionX = 11700.0
 topPositionY = 12580.0
 bottomPositionY = 12460.0
 
-with open('rutas/trazas_gps_filtradas.txt', 'rb') as csvfile:
+with open('rutas/trazas_gps_filtradas_100ms.txt', 'rb') as csvfile:
     rowReader = csv.reader(csvfile, delimiter='\t', quotechar='|')
     counter = 0
     for row in rowReader:
@@ -17,13 +17,14 @@ with open('rutas/trazas_gps_filtradas.txt', 'rb') as csvfile:
         y_position = float(row[3])
 
         if bottomPositionX <= x_position <= topPositionX and bottomPositionY <= y_position <= topPositionY:
-            print(row)
+            row.append(round(float(row[5])/3.6, 2))
             filteredTraces.append(row)
-            counter += 1
-filteredTraces = sorted(filteredTraces, key=lambda row: row[1], reverse=True)
 
-with open('rutas/important_intersection_traces.csv', mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            counter += 1
+filteredTraces = sorted(filteredTraces, key=lambda row: row[0], reverse=True)
+
+with open('rutas/important_intersection_traces_100ms.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     for trace in filteredTraces:
         employee_writer.writerow(trace)
