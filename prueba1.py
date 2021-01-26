bc71ab028384673f1b53f81792fa6f3c9ff857b0
Salomón Torres <nomolas1@gmail.com>
2021-01-25 22:49:08 -0300

    Fix meaning of print and add total routes counter

--- prueba1.py
+++ prueba1.py
@@ -3,7 +3,6 @@ import csv
 from time import sleep
 import matplotlib.pyplot as plt
 
-
 average = 0.0
 total = 0.0
 
@@ -15,12 +14,12 @@ velocidad = []
 e_velocidad = []
 e_pos_x = []
 e_pos_y = []
-#with open('pruebas/trazasGiro.csv') as csvfile:
-#with open('pruebas/pruebaxs.csv') as csvfile:
-#with open('rutas_win/single_intencion.csv') as csvfile:
-#with open('rutas/gpsWinConIntencion.csv') as csvfile:
-#with open('rutas/routes_with_intention.csv') as csvfile:
-#with open('rutas/routes_with_intention.csv') as csvfile:
+# with open('pruebas/trazasGiro.csv') as csvfile:
+# with open('pruebas/pruebaxs.csv') as csvfile:
+# with open('rutas_win/single_intencion.csv') as csvfile:
+# with open('rutas/gpsWinConIntencion.csv') as csvfile:
+# with open('rutas/routes_with_intention.csv') as csvfile:
+# with open('rutas/routes_with_intention.csv') as csvfile:
 with open('rutas_win/test_generated_routes_intencion.csv') as csvfile:
     data = [tuple(line) for line in csv.reader(csvfile, delimiter=',')]
 
@@ -36,34 +35,33 @@ false_positive = 0
 true_negative = 0
 false_negative = 0
 total_samples = 0
+total_routes = 1
 end_route = False
 detected_turn = False
 detected = False
 
-#i = 430
-#while i < 760:
-c = 0
+#i = 816
+#while i < 1134:
 while i < len(data) - 1:
-    if current_route != data[i+1][3]:
+    if current_route != data[i + 1][3]:
         end_route = True
+        total_routes += 1
         # print(data[i][5], detected_turn)
-        current_route = data[i+1][3]
+        current_route = data[i + 1][3]
         nueva_ruta = True
         detected = False
         if not detected_turn and data[i][5] == '1':
             false_negative += 1
 
         elif not detected_turn and data[i][5] == '2':
-        #elif data[i][5] == '2':
+            # elif data[i][5] == '2':
             true_negative += 1
-
 
         detected_turn = False
         # print('cambio de ruta')
         average = 0.0
         total = 0.0
         i += 2
-
 
     # print(i)
     row = data[i]
@@ -103,10 +101,9 @@ while i < len(data) - 1:
                 true_positive += 1
             elif intention == '2':
                 false_positive += 1
-        #print('Giro detectado en tiempo: ' + str(i + 1) + " Prob : " + str(current_p[1]))
-#    if current_p[1] > 0.691:
-#        detected = True
-
+        # print('Giro detectado en tiempo: ' + str(i + 1) + " Prob : " + str(current_p[1]))
+    #    if current_p[1] > 0.691:
+    #        detected = True
 
     # print(xPos, yPos, velocity)
     # print(nextPosX, nextPosY, nextVelocity)
@@ -115,16 +112,15 @@ while i < len(data) - 1:
     pal_prom += 1
     end_route = False
 total_samples = true_negative + true_positive + false_negative + false_positive
-accuracy = float(true_positive + true_negative)/total_samples
-print('Cantidad de detecciones : ' + str(counter))
+accuracy = float(true_positive + true_negative) / total_samples
+print('Cantidad de rutas: ' + str(total_routes))
+print('Cantidad de detecciones correctas: ' + str(counter))
 print('True positives : ' + str(true_positive))
 print('False positives : ' + str(false_positive))
 print('True negatives : ' + str(true_negative))
 print('False negatives : ' + str(false_negative))
 print('Total : ' + str(total_samples))
 print('Accuracy : ' + str(accuracy))
-
-
 
 plt.figure(1)
 plt.subplot(221)
@@ -137,18 +133,17 @@ plt.subplot(222)
 plt.plot(velocidad, label='real')
 plt.plot(e_velocidad, label='estimada')
 
-#plt.plot(e_pos_x, e_pos_y, label='estimada')
+# plt.plot(e_pos_x, e_pos_y, label='estimada')
 plt.legend()
 
 plt.subplot(223)
 plt.plot(velocidad, label='real')
-#plt.plot(e_pos_x, e_pos_y, label='estimada')
+# plt.plot(e_pos_x, e_pos_y, label='estimada')
 plt.legend()
 
 plt.subplot(224)
 plt.plot(realPosX, realPosY, label='real')
 plt.plot(e_pos_x, e_pos_y, label='estimada')
-
 
 fig, ax1 = plt.subplots()
 ax1.plot(realPosX, 'red', label='X position')
@@ -162,14 +157,12 @@ plt.ylabel('Probability', labelpad=5)
 fig.tight_layout()
 plt.legend()
 
-
 plt.figure(3)
 plt.plot(probGiro, label='turn probability')
 plt.plot(prom, label='average')
 plt.xlabel('Data steps [Frequency=10 Hz]', labelpad=5)
 plt.ylabel('Probability', labelpad=5)
 plt.legend()
-
 
 plt.figure(4)
 plt.plot(velocidad, label='real')
@@ -185,7 +178,7 @@ plt.xlabel('Position X', labelpad=5)
 plt.ylabel('Position Y', labelpad=5)
 plt.legend()
 
-#plt.show()
+# plt.show()
 # fig, ax1 = plt.subplots()
 #
 # ax1.plot(realPosY)
